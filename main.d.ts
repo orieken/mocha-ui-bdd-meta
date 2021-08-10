37c790f1f853e65299300bcaaaae79411d13c70e
Rieken <oscar.rieken@returnly.com>
2021-08-10 07:25:01 -0400

    :bug: (typings) added namespace

---
+++ main.d.ts
@@ -0,0 +1,79 @@
+import { Context, Done, HookFunction, Suite, Test } from "mocha";
+
+declare function MochaUiMeta(suite: any): void;
+
+declare module 'mocha-ui-bdd-meta' {
+    export = MochaUiMeta;
+}
+
+declare var beforeAll: MochaUiMeta.MetaHookFunction;
+declare var beforeEach: MochaUiMeta.MetaHookFunction;
+declare var describe: MochaUiMeta.SuiteFunction;
+declare var fdescribe: MochaUiMeta.SuiteFunction;
+declare var xdescribe: MochaUiMeta.SuiteFunction;
+declare var it: MochaUiMeta.TestFunction;
+declare var fit: MochaUiMeta.TestFunction;
+declare var xit: MochaUiMeta.TestFunction;
+declare var test: MochaUiMeta.TestFunction;
+declare var xtest: MochaUiMeta.TestFunction;
+
+declare namespace MochaUiMeta {
+    interface SuiteFunction {
+        (title: string, fn?: (this: MetaSuite) => void): MetaSuite;
+
+        (title: string, meta?: unknown, fn?: (this: MetaSuite) => void): MetaSuite;
+
+        only: ExclusiveSuiteFunction
+        skip: PendingSuiteFunction
+    }
+
+    interface MetaHookFunction extends HookFunction {
+        (fn: (this: MetaContext, done: Done) => void): void;
+
+        (fn: (this: MetaContext, done: Done) => PromiseLike<any>): void;
+
+        (name: string, fn?: (this: MetaContext, done: Done) => void): void;
+
+        (name: string, fn?: (this: MetaContext, done: Done) => PromiseLike<any>): void;
+    }
+
+    class MetaSuite extends Suite {
+        meta?: unknown;
+    }
+
+    class MetaTest extends Test {
+        meta?: unknown;
+    }
+
+    class MetaContext extends Context {
+        override test?: MetaTest | undefined;
+        meta?: unknown;
+    }
+
+    interface ExclusiveSuiteFunction {
+        (title: string, meta?: unknown, fn?: (this: MetaSuite) => void): MetaSuite;
+
+        (title: string, meta?: unknown): Suite;
+    }
+
+    interface PendingSuiteFunction {
+        (title: string, meta?: unknown, fn?: (this: MetaSuite) => void): MetaSuite | void;
+    }
+
+    interface PendingTestFunction {
+        (title: string, fn?: (this: MetaContext, done: Done) => void): Test;
+    }
+
+    interface ExclusiveTestFunction {
+        (title: string, fn?: (this: MetaContext, done: Done) => void): Test;
+    }
+
+    interface TestFunction {
+        (name: string, fn?: (this: MetaContext, done: Done) => void): MetaTest;
+
+        (name: string, meta?: unknown, fn?: (this: MetaContext, done: Done) => void): MetaTest;
+
+        only: ExclusiveTestFunction
+        skip: PendingTestFunction
+    }
+}
--- specs/sample.spec.js
+++ specs/sample.spec.js
@@ -4,32 +4,32 @@ const Level1 = { foo: 'bar' };
 const Level2 = { hi: 'bye' };
 const TestLevel = { hola: 'adios' };
 
-describe('no meta on suite',  function() {
-  it('meta on it',TestLevel, function() {
+describe('no meta on suite', function () {
+  it('meta on it', TestLevel, function () {
     expect(this.test.meta).to.eql(TestLevel);
   });
 });
 
-describe('meta on suite',Level1,  function() {
-  it('no meta on it', function() {
+describe('meta on suite', Level1, function () {
+  it('no meta on it', function () {
     expect(this.test.meta).to.eql(Level1);
   });
 
-  it('meta on it', TestLevel, function() {
-    expect(this.test.meta).to.eql({...Level1, ...TestLevel});
+  it('meta on it', TestLevel, function () {
+    expect(this.test.meta).to.eql({ ...Level1, ...TestLevel });
   });
 });
 
-describe('Level 1', Level1, function() {
-  it('it has meta from Level 1 and spec', TestLevel , function() {
+describe('Level 1', Level1, function () {
+  it('it has meta from Level 1 and spec', TestLevel, function () {
     expect(this.test.meta).to.eql({ ...Level1, ...TestLevel });
   });
-  describe('Level 2',Level2, function() {
-    it('it has meta from Level 1, level 2', function() {
-      expect(this.test.meta).to.eql({ ...Level1, ...Level2});
+  describe('Level 2', Level2, function () {
+    it('it has meta from Level 1, level 2', function () {
+      expect(this.test.meta).to.eql({ ...Level1, ...Level2 });
     });
-    it('it has meta from Level 1, level 2, and spec', TestLevel , function() {
-      expect(this.test.meta).to.eql({...Level1, ...Level2, ...TestLevel});
+    it('it has meta from Level 1, level 2, and spec', TestLevel, function () {
+      expect(this.test.meta).to.eql({ ...Level1, ...Level2, ...TestLevel });
     });
   });
 });
